f282804ec563cdfe2d1983499c33b22efc1fa24d
YuHuaiChen <tp6m4cj96@gmail.com>
2015-12-22 16:04:14 +0800

    install material-ui

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/core.scss';
 
 export default class App extends Component {
   static propTypes = {
--- app/main_client.js
+++ app/main_client.js
@@ -10,6 +10,15 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
 window.__DEBUG__ = true;
+
+const injectTapEventPlugin = require('react-tap-event-plugin');
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+// react render
 Meteor.startup(() => {
   const target  = document.getElementById('root');
   const history = createBrowserHistory();
